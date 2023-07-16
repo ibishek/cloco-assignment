0ecf7dbc9a79b0c6beb8352431c891278ac03e44
Abhishek Rijal <abhishek.rijal1999@gmail.com>
2023-07-16 17:33:09 +0545

    feat(npm packages): setup tailwindcss and vuejs

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('slug')->index();
             $table->string('email')->unique();
-            $table->string('role')->default(UserType::User->value);
+            $table->string('role')->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
