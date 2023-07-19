26e3808bcf7ac2e3e008196876bbc656b025f204
Abhishek Rijal <abhishek.rijal1999@gmail.com>
2023-07-19 22:43:18 +0545

    fix(admin contrller): register admin user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\UserTypeEnum;
+use App\Http\Requests\AdminCreateRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
-    public function register()
+    public function register(AdminCreateRequest $request)
     {
+        $slug =  User::getSlug(new User, $request->first_name, $request->last_name);
 
+        $created = DB::statement('INSERT into users(first_name, last_name, slug, email, password, gender, role, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)', [$request->first_name, $request->last_name, $slug, $request->email, Hash::make($request->password), $request->gender, UserTypeEnum::Admin->value, now(), now()]);
+
+        if ($created) {
+            return response()->json([], 204);
+        }
+
+        return response()->json([
+            'message' => 'Unable to create admin user'
+        ], 400);
     }
 }
