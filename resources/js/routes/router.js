1d50745d8e887f4b67f987156dd0fcc4184b8b3a
Abhishek Rijal <abhishek.rijal1999@gmail.com>
2023-07-20 23:41:04 +0545

    feat(vue router): use auth store to check if the is authenticated

--- resources/js/routes/router.js
+++ resources/js/routes/router.js
@@ -1,9 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes";
+import { useAuthStore } from "../stores/useAuthStore";
+import axios from "axios";
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach(async (to, from, next) => {
+    const useAuth = useAuthStore();
+    document.title = to.meta.title;
+
+    if (!useAuth.isFetched) {
+        await useAuth.fetchUser();
+    }
+
+    if (to.matched.some(route => route.meta.authRequired === true)) {
+        if (useAuth.isAuthenticated) {
+            next();
+        } else {
+            next({
+                name: 'login'
+            })
+        }
+    } else {
+        next();
+    }
 });
 
 export default router;
